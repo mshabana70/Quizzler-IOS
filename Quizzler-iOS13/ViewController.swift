ca16e3f2608b785bf289a4f2a56bc7930a0da63e
Mahmoud Shabana <mshabana5656@gmail.com>
2020-06-12 00:56:04 -0400

    connected UI element to VC

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -3,12 +3,16 @@
 //  Quizzler-iOS13
 //
 //  Created by Angela Yu on 12/07/2019.
+//  Modified by Mahmoud Shabana on 6/12/2020.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
